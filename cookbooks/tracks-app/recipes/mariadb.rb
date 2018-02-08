88ff893611bfb9690f732cbc26202738720a4574
Junior Holowka <junior.holowka@gmail.com>
2018-02-08 10:39:33 +0100

    Add mariadb recipe

--- cookbooks/tracks-app/recipes/mariadb.rb
+++ cookbooks/tracks-app/recipes/mariadb.rb
@@ -4,9 +4,13 @@
 #
 # Copyright:: 2018, Alencar Junior, All Rights Reserved.
 
+database_name     = node['mariadb']['database_name']
+database_username = node['mariadb']['database_username']
+database_password = node['mariadb']['database_password']
+
 apt_repository 'mariadb' do
   uri "http://ftp.hosteurope.de/mirror/mariadb.org/repo/10.2/#{node['platform']}"
-  distribution "#{node['lsb']['codename']}"
+  distribution (node['lsb']['codename']).to_s
   components ['main']
   keyserver 'hkp://keyserver.ubuntu.com:80'
   key '0xcbcb082a1bb943db'
@@ -22,4 +26,12 @@ service 'mysql' do
   action   [:enable, :start]
 end
 
+execute 'create-database' do
+  command "mysql -u root -e 'CREATE DATABASE IF NOT EXISTS #{database_name} CHARACTER SET utf8'"
+  user 'root'
+end
 
+execute 'create-database-user' do
+  command "mysql -u root -e \"CREATE USER IF NOT EXISTS '#{database_username}'@'localhost' IDENTIFIED BY '#{database_password}'; GRANT ALL PRIVILEGES ON *.* TO '#{database_username}'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES\""
+  user 'root'
+end
