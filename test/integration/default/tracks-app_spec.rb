8647a9e6e96c54ba6a3746852c3a6e16eadd50e2
Junior Holowka <junior.holowka@gmail.com>
2018-02-08 18:40:48 +0100

    Add tests

--- test/integration/default/tracks-app_spec.rb
+++ test/integration/default/tracks-app_spec.rb
@@ -1,5 +1,12 @@
-describe package('nginx') do
+
+describe command('lsb_release --description --short') do
+  its('stdout') { should match 'Ubuntu 14.04.5 LTS' }
+end
+
+describe service('postgresql') do
+  it { should be_enabled }
   it { should be_installed }
+  it { should be_running }
 end
 
 describe service('nginx') do
@@ -8,6 +15,17 @@ describe service('nginx') do
   it { should be_running }
 end
 
-describe command("curl localhost") do
-  its("stdout") { should match "hello world" }
+describe command('ruby --version') do
+  its('stdout') { should match 'ruby 2.1' }
 end
+
+describe port(22) do
+  its('processes') { should include 'sshd' }
+  its('protocols') { should include 'tcp' }
+  its('addresses') { should include '0.0.0.0' }
+end
+
+describe host('localhost', port: 80, protocol: 'tcp') do
+  it { should be_reachable }
+end
+
