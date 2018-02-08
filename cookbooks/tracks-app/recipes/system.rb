f8cdcb2602c7c5268220983c17d2902d9d226555
Junior Holowka <junior.holowka@gmail.com>
2018-02-08 10:45:36 +0100

    Add locale settings

--- cookbooks/tracks-app/recipes/system.rb
+++ cookbooks/tracks-app/recipes/system.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2018, Alencar Junior, All Rights Reserved.
 
+timezone = node['system']['timezone']
 
 # update apt cache daily
 apt_update 'daily' do
@@ -12,15 +13,16 @@ apt_update 'daily' do
 end
 
 # install essential packages
-package %w(software-properties-common make git-core zlib1g zlib1g-dev libsqlite3-dev)
+package %w(build-essential curl git-core libcrypto++-dev libcurl4-openssl-dev libffi-dev libgmp-dev libgmp3-dev libmariadbd-dev libnetcdf-dev libpq-dev libreadline-dev libsqlite3-dev libsqlite3-dev libssl-dev libxml2-dev libxslt1-dev libyaml-dev make python-software-properties software-properties-common zlib1g zlib1g-dev)
 
-
-# timezone setup
-execute "timezone_setup" do
-  command 'echo "Etc/UTC" > /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata'
+execute 'timezone_setup' do
+  command "echo #{timezone} > /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata"
   user 'root'
 end
 
-
+execute 'install_english_locale' do
+  command "locale-gen en_US.UTF-8 && locale-gen en_US && locale-gen"
+  user 'root'
+end
 
 
