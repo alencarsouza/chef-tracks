ac8abfbc4ec33fd32d775c5b43e59ea3211bd257
Alencar Junior <junior.holowka@gmail.com>
2018-02-09 14:27:57 +0100

    Fix secret should have least 30 characters

--- cookbooks/tracks-app/attributes/default.rb
+++ cookbooks/tracks-app/attributes/default.rb
@@ -3,7 +3,7 @@ default['application']['app_repository'] = 'https://github.com/TracksApp/tracks.
 default['application']['app_branch']     = '2.3_branch'
 default['application']['app_name']       = 'tracks '
 default['application']['document_root']  = '/srv/www/tracks'
-default['application']['token']          = '456789435345879'
+default['application']['token']          = 'c2e46be6e2601df7251f2d90cbb7abedb828b8bd0f0a13e8913c474e33b3701c385c470feb3541bbd85c37ac5ea1f1fbf97e345075a1fed5c4a181817f0b375c'
 
 # database settings
 default['postgresql']['database_adapter']  = 'postgresql'
